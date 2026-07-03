25967bb248dca83c84634536c78b1e9f2d55f237
Sai kiran <saikiran29.u@gmail.com>
2026-07-04 03:06:38 +0530

    delete: remove index.js and index2.js files

--- index.js
+++
@@ -1,8 +0,0 @@
-function add(a, b) {
-    return a + b;
-}   
-
-add(5, 3); // This will return 8
-add(10, 15); // This will return 25
-add(-2, 7); // This will return 5
-add(0, 0); // This will return 0
--- index2.js
+++
@@ -1,6 +0,0 @@
-console.log('hey');
-
-function add(a, b) {
-    return a + b;
-}   
-add(2, 3);
