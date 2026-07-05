b505f0a17be42f1590937175c166dc5d7aaa8758
Sai kiran <saikiran29.u@gmail.com>
2026-07-05 17:28:49 +0530

    add: implement subtract and multiply functions in git.js

--- git.js
+++ git.js
@@ -2,3 +2,14 @@ function add(a, b) {
     return a + b;
 }   
 add(2, 3);
+
+
+ function subtract(a, b) {
+    return a - b;
+}   
+subtract(5, 2);
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply(4, 3);
